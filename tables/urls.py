2fc521ed0c4314146878f5d176963d92db1f2835
DeanArchi <ivan.artsibarskiy@gmail.com>
2023-10-24 13:27:06 +0300

    authentication added

--- tables/urls.py
+++ tables/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('select_table/', views.select_table, name='select_table'),
+    path('', views.select_table, name='tables'),
 ]
